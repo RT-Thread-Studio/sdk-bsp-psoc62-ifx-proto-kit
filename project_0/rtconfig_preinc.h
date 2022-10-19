d0981e9da10930d521574f16150aab16d25aa8c0
guozhanxin <guozhanxin@rt-thread.com>
2022-10-19 11:48:57 +0800

    first version.

---
+++ project_0/rtconfig_preinc.h
@@ -0,0 +1,20 @@
+
+#ifndef RTCONFIG_PREINC_H__
+#define RTCONFIG_PREINC_H__
+
+/* Automatically generated file; DO NOT EDIT. */
+/* RT-Thread pre-include file */
+
+#define COMPONENT_BSP_DESIGN_MODUS
+#define COMPONENT_CAT1
+#define COMPONENT_CAT1A
+#define CY8C624ABZI_S2D44
+#define CY_USING_HAL
+#define HAVE_CCONFIG_H
+#define IFX_PSOC6_43012
+#define RT_USING_LIBC
+#define RT_USING_NEWLIB
+#define _POSIX_C_SOURCE 1
+#define __RTTHREAD__
+
+#endif /*RTCONFIG_PREINC_H__*/
